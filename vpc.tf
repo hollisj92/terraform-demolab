277831647feba0c420ef88a0aa92db4dffbde552
JHollis <85080306+hollisj92@users.noreply.github.com>
2022-11-22 21:36:10 -0500

    working commit

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,17 @@
 resource "aws_vpc" "demo_vpc" {
+    cidr_block = var.cidr-cidr_block[0].cidr_block
+    tags = {
+      "Environment" = "Production"
+      Name : "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_subnet" "demo_subnet-1" {
+    vpc_id = aws_vpc.demo_vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.az
+    tags = {
+        Name : "${var.env_prefix}-subnet-1"
+    }
     
 }
