3ccd9810988797222d4841f8c621dc3899ab7b99
JHollis <85080306+hollisj92@users.noreply.github.com>
2022-11-22 22:30:04 -0500

    working commit

--- compute.tf
+++ compute.tf
@@ -6,21 +6,21 @@ resource "aws_security_group" "demo_sg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = var.my_ip
+        cidr_blocks = [var.my_ip]
     }
 
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = var.my_ip
+        cidr_blocks = [var.my_ip]
     }
 
     egress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
-        cidr_blocks = var.open_cidr
+        cidr_blocks = [var.open_cidr]
     }
 
 
