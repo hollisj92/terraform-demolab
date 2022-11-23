3ba4a5850b7f065052e53de5dc51d17b0bfd998f
JHollis <85080306+hollisj92@users.noreply.github.com>
2022-11-23 16:28:46 -0500

    working commit

--- compute.tf
+++ compute.tf
@@ -82,10 +82,3 @@ data "aws_ami" "updated_ami" {
 
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.updated_ami.id
-}
-
-output "aws_ec2_lab1_pubip" {
-  value = resource.aws_instance.lab1.public_ip
-}
---
+++ modules/routing/main.tf
@@ -0,0 +1,55 @@
+resource "aws_internet_gateway" "demo_igw" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  tags = {
+        Env: "${var.env_prefix}"
+        Service: "${var.env_prefix}-igw-demo"
+        Name : "${var.env_prefix}-igw"
+        Role: "${var.env_prefix}-igw"
+        Team: "team-${var.team}"
+    
+  }
+}
+
+resource "aws_vpc" "demo_vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+      Name : "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_route_table" "demo_route_table" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  route {
+    cidr_block = var.open_cidr
+    gateway_id = aws_internet_gateway.demo_igw.id
+  }
+
+  tags = {
+          Env: "${var.env_prefix}"
+          Service: "${var.env_prefix}-route_tbl-demo"
+          Name : "${var.env_prefix}-route_tbl"
+          Role: "${var.env_prefix}-route_tbl"
+          Team: "team-${var.team}"
+      
+  }
+}
+
+resource "aws_subnet" "demo_subnet_1" {
+    vpc_id = aws_vpc.demo_vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.az
+    tags = {
+        Env: "${var.env_prefix}"
+        Service: "${var.env_prefix}-subnet-demo"
+        Name : "${var.env_prefix}-subnet1"
+        Role: "${var.env_prefix}-subnet"
+        Team: "team-${var.team}"
+    }
+}
+
+resource "aws_route_table_association" "subnet_2_rtbl_assc" {
+  subnet_id      = aws_subnet.demo_subnet_1.id
+  route_table_id = aws_route_table.demo_route_table.id
+}
