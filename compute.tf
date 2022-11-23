80aa0f561385d09d7c4647de5cf8a658f612095d
JHollis <85080306+hollisj92@users.noreply.github.com>
2022-11-23 13:07:20 -0500

    working commit

--- compute.tf
+++ compute.tf
@@ -1,6 +1,36 @@
-# resource "aws_instance" "lab1" {
+resource "aws_instance" "lab1" {
+  ami = "${data.aws_ami.updated_ami}"
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.demo_subnet_1.id
+  vpc_security_group_ids = [ aws_security_group.demo_sg.id ]
+  availability_zone = var.az
+  associate_public_ip_address = true
+
+  user_data = file("entry-script.sh")
+
+  # provisioner "file" {
+  #   source = "entry-script.sh"
+  #   destination = "/home/ec2-user/ec2-entry-script.sh"
+  # }
+
+  # provisioner  "remote-exec" {
+  #   script = file("ec2-entry-script.sh")
+  # }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > output.txt"
+    
+  }
   
-# }
+  tags = {
+        Env: "${var.env_prefix}"
+        Service: "${var.env_prefix}-lab-demo"
+        Name : "${var.env_prefix}-Lab1"
+        Role: "${var.env_prefix}-webserver"
+        Team: "team-${var.team}"
+    
+    }
+}
 
 resource "aws_security_group" "demo_sg" {
     vpc_id = aws_vpc.demo_vpc.id
@@ -39,9 +69,9 @@ data "aws_ami" "updated_ami" {
     most_recent = true
     owners = ["amazon"]
 
-    filter {
-      name   = "name"
-      values = ["amzn2-ami-kernel-5.10-hvm-*"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*"]
   }
 
   filter {
--- vars.tf
+++ vars.tf
@@ -3,6 +3,7 @@ variable "region" {
     sensitive = true
     type = string
 }
+# ---------------- sensistive info
 variable "access_key" {
     description = "access_key"
     sensitive = true
@@ -13,16 +14,7 @@ variable "secret_key" {
     sensitive = true
     type = string
 }
-variable "my_ip" {
-    description = "my_ip"
-    sensitive = true
-    type = string
-}
-variable "ec2_ami" {
-    description = "ec2_ami"
-    sensitive = true
-    type = string
-}
+# ---------------- vpc/subnet/routing
 variable "vpc_cidr_block" {
     description = "vpc_cidr_block"
     sensitive = true
@@ -43,9 +35,31 @@ variable "env_prefix" {
     sensitive = true
     type = string
 }
+# ---------------- compute
+variable "instance_type" {
+    description = "instance_type"
+    sensitive = true
+    type = string
+}
+variable "ec2_ami" {
+    description = "ec2_ami"
+    sensitive = true
+    type = string
+}
 # ---------------- shortcuts
 variable "open_cidr" {
   description = "open cidrblock for shorthand"
     sensitive = false
+    type = string
+}
+variable "my_ip" {
+    description = "my_ip"
+    sensitive = true
+    type = string
+}
+# ---------------- common tags
+variable "team" {
+    description = "Team"
+    sensitive = true
     type = string
 }
