538cea9fd746c3fc71377e3567ec096a6d3d10a4
JHollis <85080306+hollisj92@users.noreply.github.com>
2022-11-25 13:58:21 -0500

    adding remote state via s3 bucket

--- entry-script.sh
+++ entry-script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo apt upgrade -y
+sudo apt-get upgrade -y
 sudo apt-get update -y && sudo snap install docker
 sudo addgroup --system docker
 sudo adduser $USER docker
 newgrp docker
-sudo docker run hello-world
 sudo snap start docker
 sudo usermod -aG docker ubuntu
 docker run -p 8080:80 nginx
